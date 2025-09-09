0195ce46035108866bc90bdf6bce4cb385ce648d
unknown <sharma.803061@gmail.comProfile image>
2025-09-09 07:52:38 +0600

    added a six file

--- script/index.js
+++ script/index.js
@@ -1,35 +1,4 @@
 
-const loadLessons = () => {
-    fetch("https://openapi.programming-hero.com/api/categories")
-    .then((res) => res.json())
-    .then((json) => displayLessons(json.categories))
-};
-
-const loadLevelWord = (id) => {
-    console.log(id);
-}
-
-const displayLessons = (lessons) => {
-    // 1. get the container & empty 
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML + "";
-
-    // 2. get into evey lessons 
-    for (let lesson of lessons) {
-       console.log(lesson);
-        // 3. create Element 
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.category_name})" class="btn btn-outline  ">
-         ${lesson.category_name}
-        </button>
-        `;
-    //    // 4. append into container 
-       levelContainer.append(btnDiv);
-    }
-         
-         
-};
-loadLessons();
 
 
 
